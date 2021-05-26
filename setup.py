1651b97f1af6dcdb58742a8dc95e9312dd963b19
Brendon Jones <brendonj@waikato.ac.nz>
2021-05-26 15:53:31 +1200

    Bump version to 1.1.0-1 and update changelog.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='amplet2-collector',
-        version='1.0',
+        version='1.1',
         description='AMP Time Series Collector',
         author='Brendon Jones',
         author_email='brendonj@waikato.ac.nz',
